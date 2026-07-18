81e10ad93c8dd1584b388101d9bca9c74e08d083
Rohan_ro23 <rohanms0405@gmail.com>
2026-07-18 09:44:23 +0530

    Initial React + TypeScript setup

---
+++ src/main/java/com/rohanv2/Rohanv2Application.java
@@ -0,0 +1,13 @@
+package com.rohanv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rohanv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rohanv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohanv2/Rohanv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohanv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rohanv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
